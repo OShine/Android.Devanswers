2fb631aa5d5330452d6c609e846c5d44da55599e
OShine <shklyannik.denis@gmail.com>
2013-12-05 02:54:21 +0300

    Ver 1.08

--- Devanswers/gen/com/example/devanswers/R.java
+++ Devanswers/gen/com/example/devanswers/R.java
@@ -32,11 +32,10 @@ public final class R {
     public static final class id {
         public static final int DevMainImage=0x7f090004;
         public static final int DevMainImage2=0x7f090002;
-        public static final int Linkid=0x7f090005;
         public static final int NodeText=0x7f090003;
         public static final int RelativeLayout=0x7f090000;
-        public static final int i_about=0x7f090006;
-        public static final int i_share=0x7f090007;
+        public static final int i_about=0x7f090005;
+        public static final int i_share=0x7f090006;
         public static final int logotext=0x7f090001;
     }
     public static final class layout {
