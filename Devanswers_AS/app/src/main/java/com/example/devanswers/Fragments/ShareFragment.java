e37d26f38fb905988db2b0d403abccd3d6d79eae
Heman055 <i.gotovchits@gmail.com>
2015-03-20 14:17:07 +0300

    Added base fragment implementation.

---
+++ Devanswers_AS/app/src/main/java/com/example/devanswers/Fragments/ShareFragment.java
@@ -0,0 +1,32 @@
+package com.example.devanswers.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.devanswers.R;
+
+public class ShareFragment extends Fragment
+{
+    public ShareFragment()
+    {
+        
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.share_layout, container, false);
+
+        return view;
+    }
+}
