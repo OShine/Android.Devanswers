73776e9910b75a5bdd0ca787d58d37b5db3c501b
Heman055 <i.gotovchits@gmail.com>
2015-03-20 14:38:37 +0300

    Initialized base components in main activity.

--- Devanswers_AS/app/src/main/java/com/example/devanswers/Activities/MainActivity.java
+++ Devanswers_AS/app/src/main/java/com/example/devanswers/Activities/MainActivity.java
@@ -1,19 +1,70 @@
 package com.example.devanswers.Activities;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.devanswers.Fragments.CopyrightFragment;
+import com.example.devanswers.Fragments.ShareFragment;
+import com.example.devanswers.HttpManager.HttpManager;
+import com.example.devanswers.InternetManager.InternetManager;
 import com.example.devanswers.R;
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity implements View.OnClickListener
 {
-    private RelativeLayout _clickableArea;
+    private InternetManager _internetManager;
+
+    private HttpManager _httpManager;
+
+    private ShareFragment _shareFragment;
+    private CopyrightFragment _copyrightFragment;
+
+    private ImageButton _requestAnswer;
+
+    private ImageView _answerLoading;
+
+    private TextView _textAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        _internetManager = new InternetManager(getApplicationContext());
+
+        _httpManager = new HttpManager();
+
+        _requestAnswer = (ImageButton) findViewById(R.id.request_answer_ImageButton);
+        _requestAnswer.setOnClickListener(this);
+
+        _answerLoading = (ImageView) findViewById(R.id.answer_loading_ImageView);
+
+        _textAnswer = (TextView) findViewById(R.id.text_answer_TextView);
+
+        _shareFragment = (ShareFragment) getSupportFragmentManager().findFragmentById(R.id.share_fragment);
+        _copyrightFragment = (CopyrightFragment) getSupportFragmentManager().findFragmentById(R.id.copyright_fragment);
+
+        getSupportFragmentManager().beginTransaction()
+                                   .hide(_shareFragment)
+                                   .hide(_copyrightFragment)
+                                   .commit();
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.request_answer_ImageButton:
+                if (_internetManager.IsConnected() == true)
+                {
+                    //answer request
+                }
+                break;
+        }
     }
 }
